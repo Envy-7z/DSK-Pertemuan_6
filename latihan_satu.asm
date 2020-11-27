618e4fa34bb3ee92e9f7ba3d6369668a6fe1873e
Wisnu Andrian <49360795+Envy-7z@users.noreply.github.com>
2020-11-28 02:36:58 +0700

    Update latihan_satu.asm

--- latihan_satu.asm
+++ latihan_satu.asm
@@ -1,14 +1,18 @@
+;=================================;
+; PROGRAM : A1.ASM ;
+; FUNGSI : MENCETAK KARATER 'A';
+; BESERTA ATRIBUTNYA ;
+; DENGAN INT 10h ;
+;============================S'to=;
 .MODEL SMALL
 .CODE
 ORG 100h
-Proses:
-    MOV AH , 09H  
-    MOV AL , 'A'
-    MOV BH , 00h
-    MOV BL , 93h
-    MOV CX , 03h
-    INT 10h 
-
-    INT 20h
-
+Proses :
+MOV AH,09h ; Nilai servis untuk mencetak karakter
+MOV AL,'A' ; AL = Karakter yang akan dicetak
+MOV BH,00h ; Nomor Halaman layar
+MOV BL,93h ; Warna atau atribut dari karakter
+MOV CX,03h ; Banyaknya karakter yang ingin dicetak
+INT 10h ; Laksanakan !!!
+INT 20h ; Selesai ! kembali ke DOS
 END Proses
